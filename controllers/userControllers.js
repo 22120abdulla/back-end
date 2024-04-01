6e86ad885e8e051d79906c107b1016c75a8ce941
nv22120 <154723822+22120abdulla@users.noreply.github.com>
2024-04-01 17:42:43 +0000

    16 - Login & User profile

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,9 +8,8 @@ export const registerUser = async (req, res, next) => {
     let user = await User.findOne({ email });
 
     if (user) {
-      // return res.status(400).json({ message: "User have already registered" });
       throw new Error("User have already registered");
-        }
+    }
 
     // creating a new user
     user = await User.create({
@@ -18,7 +17,6 @@ export const registerUser = async (req, res, next) => {
       email,
       password,
     });
-
     return res.status(201).json({
       _id: user._id,
       avatar: user.avatar,
@@ -33,4 +31,55 @@ export const registerUser = async (req, res, next) => {
   }
 };
 
-export { registerUser };
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("Email not found");
+    }
+
+    if (await user.comparePassword(password)) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+        token: await user.generateJWT(),
+      });
+    } else {
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const userProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
+
+    if (user) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+      });
+    } else {
+      let error = new Error("User not found");
+      error.statusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { registerUser, loginUser };
